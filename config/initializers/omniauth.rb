15a287f505d56e2abfeff3716401e54b4d6fee04
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-05-09 02:21:03 -0500

    updated tokens

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :starterleague, ENV['STARTERLEAGUE_APP_ID'], ENV['STARTERLEAGUE_APP_SECRET']
+  provider :starterleague, '026f68feec3d856b11a2af8d4a8aef727f24d620b4e78df9970f0a7ba1c6b31e', '7d50abefcbcb3851b1095722e5cefe11ecacf5fa02122f503c6e52a6e89b0ef6'
 end
+
+# ENV['STARTERLEAGUE_APP_ID'], ENV['STARTERLEAGUE_APP_SECRET']
